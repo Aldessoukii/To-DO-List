95f3dcbdf5889cb1ba4372ef13a2ca8a87d3dbe8
aldessoukii <aaldessoukii@gmail.com>
2025-10-01 23:33:28 +0300

    edit the script and css

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-let taskInput = document.querySelector('#taskInput');
-let addBtn = document.querySelector('#addbtn');
-let taskList = document.querySelector('#taskList');
+let addBtn = document.getElementById("addbtn");
+let input = document.getElementById("inputlist");
+let ul = document.getElementById("ullist");
+addBtn.addEventListener("click", function () {
+  let task = input.value.trim();
+  if (task === "") {
+    alert("Please enter a task!");
+    return;
+  }
+  let li = document.createElement("li");
+  li.textContent = task;
 
+  let delBtn = document.createElement("button");
+  delBtn.textContent = "Delete";
+  delBtn.addEventListener("click", function () {
+    li.remove();
+  });
 
+  li.appendChild(delBtn);
+
+  ul.appendChild(li);
+
+  input.value = "";
+});
+p;
